b2bdc4c805930adc10f85b3e45434519b57f5455
taeng93 <firbigi1993@gmail.com>
2018-12-14 14:34:50 +0900

    random size make color none stroke

--- snowing.jsx
+++ snowing.jsx
@@ -10,25 +10,39 @@ var doc_h = myDoc.height;
 $.writeln ("doc_w : " + doc_w);
 $.writeln ("doc_h : " + doc_h);
 
+//add layer
+var layer = myDoc.layers.add();
+layer.name = "snow";
+
 //draw snow
 var cir = myDoc.pathItems;
-var size = 10;
+var size = 4;
 var count = 30;
+var snowColor = new GrayColor();
+snowColor.gray = 0;
+
 
 for(var i=0; i<count; i++){
+    
+    
     var temp_x = Math.random()*doc_w;
     var temp_y = Math.random()*doc_h*-1;
+    var temp_size = size + Math.random()*8;
     $.writeln ("temp_x : " + temp_x);
     $.writeln ("temp_y : " + temp_y);
     
     //top, left, width, height
-    var cir_temp = cir.ellipse(temp_y, temp_x,size,size);
+    var cir_temp = cir.ellipse(temp_y, temp_x,temp_size,temp_size);
+    cir_temp.filled = true;
+    cir_temp.fillColor = snowColor;
+    cir_temp.stroked = false;
+    
    // var test = cir_temp .graphicStyles.length;
-     $.writeln ("cir_temp : " + cir_temp);
+    $.writeln ("cir_temp : " + cir_temp);
+    
     }
 
-
-
+//snow hill
 
 
 
